a37e22c63a0c9f7e4bb224b4da09fee37124eed8
Joao Moreira <joaomarcello.mm@gmail.com>
2024-02-18 13:36:23 -0300

    showing player hp and ammo

--- lib/gamestate/game.lua
+++ lib/gamestate/game.lua
@@ -1,6 +1,7 @@
 local path = ...
 local JM = _G.JM
 local Kid = require "lib.object.kid"
+local DisplayHP = require "lib.object.displayHP_game"
 
 do
     _G.SUBPIXEL = _G.SUBPIXEL or 3
@@ -34,18 +35,27 @@ end
 
 local imgs
 local function load()
+    local font = JM:get_font("pix8")
+    font:set_color(JM_Utils:get_rgba(JM_Utils:hex_to_rgba_float("242833")))
+
+    local font = JM:get_font("pix5")
+    font:set_color(JM_Utils:get_rgba(JM_Utils:hex_to_rgba_float("242833")))
+
     Kid:load()
+    DisplayHP:load()
 
     local lgx = love.graphics
     imgs = imgs or {
         ["field"] = lgx.newImage("/data/img/background.png"),
         ["street_down"] = lgx.newImage("/data/img/back_down.png"),
         ["street_up"] = lgx.newImage("/data/img/back_up.png"),
+        ["box"] = lgx.newImage("/data/img/box_gui.png"),
     }
 end
 
 local function finish()
     Kid:finish()
+    DisplayHP:finish()
 
     if imgs then
         imgs["field"]:release()
@@ -66,11 +76,10 @@ local function init(args)
     JM.Physics:newBody(data.world, 0, 0, SCREEN_WIDTH, 16 * 3, "static")
     JM.Physics:newBody(data.world, 0, SCREEN_HEIGHT - 16, SCREEN_WIDTH, 16, "static")
 
-    State:add_object(Kid:new(16 * 12, SCREEN_HEIGHT * 0.5, Kid.Gender.boy, -1))
+    ---@type Kid
+    local k = State:add_object(Kid:new(16 * 12, SCREEN_HEIGHT * 0.5, Kid.Gender.boy, -1, true))
 
-    -- data.layer_shadow = State:newLayer {
-
-    -- }
+    data.displayHP = DisplayHP:new(data.player)
 end
 
 local function textinput(t)
@@ -125,9 +134,15 @@ end
 local function gamepadaxis(joystick, axis, value)
 end
 
+local function resize(w, h)
+    return _G.RESIZE(State, w, h)
+end
+
 local function update(dt)
     data.world:update(dt)
     State:update_game_objects(dt)
+
+    data.displayHP:update(dt)
 end
 
 local sort_draw = function(a, b)
@@ -167,8 +182,26 @@ local function draw(cam)
 
     State:draw_game_object(cam, nil, sort_draw)
 
-    lgx.setColor(1, 1, 1)
+    lgx.setColor(JM_Utils:hex_to_rgba_float("799299"))
     lgx.draw(imgs["street_down"])
+
+    local font = JM:get_font("pix5")
+    local px = data.displayHP.x
+    local py = data.displayHP.y + 8
+
+    lgx.setColor(1, 1, 1, 0.75)
+    lgx.draw(imgs["box"], px - 12, data.displayHP.y - 4)
+
+    if data.player.stones == data.player.max_stones then
+        font:printx(string.format("ammo x %d <effect=flickering, speed=0.5><color-hex=ff0000>max", data.player.stones),
+            px, py)
+    elseif data.player.stones <= 0 then
+        font:printx(string.format("<effect=flickering, speed=0.5><color-hex=ff0000>no ammo"), px, py)
+    else
+        font:printf(string.format("ammo x %d", data.player.stones), px, py)
+    end
+
+    data.displayHP:draw()
 end
 
 --============================================================================
@@ -187,6 +220,7 @@ State:implements {
     gamepadpressed = gamepadpressed,
     gamepadreleased = gamepadreleased,
     gamepadaxis = gamepadaxis,
+    resize = resize,
     update = update,
     draw = draw,
 }
