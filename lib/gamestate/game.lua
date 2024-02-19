303f1f52b9078e538ac828e1781d8820d87cafe5
Joao Moreira <joaomarcello.mm@gmail.com>
2024-02-19 16:46:18 -0300

    star working on game_logic

--- lib/gamestate/game.lua
+++ lib/gamestate/game.lua
@@ -23,12 +23,109 @@ local State = JM.Scene:new {
     cam_scale = 1,
     use_canvas_layer = true,
 }
+
+---@enum GameState.Game.States
+local States = {
+    game = 1,
+    sendKidsAway = 2,
+    waveIsComing = 3,
+    playerIsDead = 4,
+    dialogue = 5,
+}
 --============================================================================
 ---@class GameState.Game.Data
 local data = {}
 
 function data.play_song()
 
+end
+
+function data:unlock_kids()
+    for i = 1, #self.kids do
+        ---@type Kid
+        local k = self.kids[i]
+        if k
+            and k.state == Kid.State.idle
+            and k:is_on_target_position()
+        then
+            k:set_state(Kid.State.normal)
+        end
+    end
+
+    if self.player.state == Kid.State.idle
+        and self.player:is_on_target_position()
+    then
+        self.player:set_state(Kid.State.normal)
+    end
+end
+
+function data:all_kids_on_position()
+    local list = self.kids
+    local player = self.player
+
+    if not list or not player:is_on_target_position() then return false end
+    for i = 1, #list do
+        ---@type Kid
+        local k = list[i]
+        if not k:is_on_target_position() then
+            return false
+        end
+    end
+    return true
+end
+
+function data:put_kids_to_run(send_leader)
+    local list = self.kids
+    if not list then return end
+
+    for i = 1, #list do
+        ---@type Kid
+        local k = list[i]
+
+        if k ~= self.leader or send_leader then
+            k:set_state(Kid.State.runAway)
+        end
+    end
+end
+
+function data:start_countdown(init)
+    self.countdown_time = init or 1
+end
+
+function data:start_game()
+    if self.countdown_time and self.countdown_time <= 0
+        and self:all_kids_on_position()
+    then
+        self:unlock_kids()
+        self.countdown_time = nil
+        self:set_state(States.game)
+        return true
+    end
+    return false
+end
+
+function data:wave_is_over()
+    local list = self.kids
+    if not list then return false end
+
+    local c = 0
+    local N = #list
+    for i = 1, N do
+        ---@type Kid
+        local k = list[i]
+        if k:is_dead() then
+            c = c + 1
+        end
+    end
+    return c == N
+end
+
+---@param new_state GameState.Game.States
+function data:set_state(new_state)
+    if new_state == self.gamestate then return false end
+    self.gamestate = new_state
+
+    return true
 end
 
 --============================================================================
@@ -67,27 +164,103 @@ local function finish()
         imgs["street_up"]:release()
     end
     imgs = nil
+
+    data.kids = nil
+    data.world = nil
+    data.player = nil
 end
 
+local function load_wave(value)
+    value = value or 1
+
+    if data.kids then
+        local list = data.kids
+        for i = 1, #list do
+            ---@type Kid
+            local k = list[i]
+
+            if k ~= data.leader then
+                k:set_state(Kid.State.runAway)
+            end
+        end
+    end
+
+    data.kids = {}
+
+    if value == 1 then
+        ---@type Kid
+        local k = data.leader or State:add_object(Kid:new(SCREEN_WIDTH, 16 * 7, Kid.Gender.boy, -1, true, 1))
+        k:set_position(SCREEN_WIDTH, 16 * 7)
+        k:set_target_position(16 * 12)
+        k:set_state(k.State.preparing)
+        k.time_move_y = 0.0
+        k.anchor_y = 16 * 7
+        k.goingTo_speed = 1.5
+        table.insert(data.kids, k)
+        data.leader = k
+
+        k = State:add_object(Kid:new(16 * 12, 16 * 4, Kid.Gender.boy, -1, true, 2))
+        k:set_position(SCREEN_WIDTH, 16 * 4)
+        k:set_target_position(16 * 14, 16 * 4)
+        k:set_state(k.State.preparing)
+        k.goingTo_speed = 0.75
+        table.insert(data.kids, k)
+
+        -- ---@type Kid
+        -- k = State:add_object(Kid:new(16 * 15, 16 * 9, Kid.Gender.boy, -1, true, 3))
+        -- k:set_position(SCREEN_WIDTH, 16 * 9)
+        -- k:set_target_position(16 * 14, 16 * 9)
+        -- k:set_state(k.State.preparing)
+        -- k.goingTo_speed = 2
+        -- k.anchor_y = 16 * 9 - k.move_y_value
+        -- k.time_move_y = math.pi * 0.5
+        -- k.anchor_x = 16 * 14
+        -- k.time_move_x = 0.0
+        -- table.insert(data.kids, k)
+
+        -- ---@type Kid
+        -- k = State:add_object(Kid:new(16 * 18, 16 * 5, Kid.Gender.boy, -1, true, 1))
+        -- k:set_position(SCREEN_WIDTH, 16 * 5)
+        -- k:set_target_position(16 * 18, 16 * 5)
+        -- k:set_state(k.State.preparing)
+        -- k.goingTo_speed = 2
+        -- k.anchor_y = 16 * 5 + k.move_y_value
+        -- k.time_move_y = -math.pi * 0.5
+        -- table.insert(data.kids, k)
+        ---
+    elseif value == 2 then
+    else
+    end
+end
+
+
 local function init(args)
+    args = args or {}
+
+    data.time_game = 0
+
     data.world = JM.Physics:newWorld { tile = TILE }
     JM.GameObject:init_state(State, data.world)
     State.game_objects = {}
 
-    data.player = Kid:new(nil, SCREEN_HEIGHT * 0.5, 1)
+    data.player = Kid:new(16 * 7, 16 * 7, 1)
+    data.player:set_position(-50, 16 * 7)
+    data.player:set_state(Kid.State.preparing)
+    data.player.goingTo_speed = 2
+
     State:add_object(data.player)
 
     JM.Physics:newBody(data.world, 0, 0, SCREEN_WIDTH, 16 * 3, "static")
     JM.Physics:newBody(data.world, 0, SCREEN_HEIGHT - 16, SCREEN_WIDTH, 16, "static")
 
-    ---@type Kid
-    local k = State:add_object(Kid:new(16 * 13, 16 * 7, Kid.Gender.boy, -1, true, 1))
-    -- k:set_position(16 * 14, 16 * 9)
-
-    k = State:add_object(Kid:new(16 * 12, 16 * 4, Kid.Gender.boy, -1, true, 2))
-    k = State:add_object(Kid:new(16 * 15, 16 * 9, Kid.Gender.boy, -1, true, 3))
+    data.leader = nil
+    data.wave_number = args.wave_number or 1
+    load_wave(data.wave_number)
 
     data.displayHP = DisplayHP:new(data.player)
+
+    data:start_countdown(0.3)
+    data:set_state(States.waveIsComing)
 end
 
 local function textinput(t)
@@ -207,7 +380,40 @@ local function resize(w, h)
     return _G.RESIZE(State, w, h)
 end
 
+local function game_logic(dt)
+    if data:wave_is_over() then
+
+    end
+end
+
 local function update(dt)
+    data.time_game = data.time_game + dt
+    if data.countdown_time then
+        data.countdown_time = data.countdown_time - dt
+        if data.countdown_time <= 0 then
+            data:start_game()
+        end
+    end
+
+    -- if data.time_game >= 2 then
+    --     for i = 1, #data.kids do
+    --         ---@type Kid
+    --         local k = data.kids[i]
+    --         if k
+    --             and k.state == Kid.State.idle
+    --             and k:is_on_target_position()
+    --         then
+    --             k:set_state(Kid.State.normal)
+    --         end
+    --     end
+
+    --     if data.player.state == Kid.State.idle
+    --         and data.player:is_on_target_position()
+    --     then
+    --         data.player:set_state(Kid.State.normal)
+    --     end
+    -- end
+
     data.world:update(dt)
     State:update_game_objects(dt)
 
@@ -270,6 +476,12 @@ local function draw(cam)
         font:printf(string.format("ammo x %d", data.player.stones), px, py)
     end
 
+    font = JM:get_font("pix8")
+    font:print(tostring(#State.game_objects), 16, 16 * 4)
+
+    if data:wave_is_over() then
+        font:printf("WAVE IS OVER", 0, 16 * 4, SCREEN_WIDTH, "center")
+    end
     data.displayHP:draw()
 end
 
