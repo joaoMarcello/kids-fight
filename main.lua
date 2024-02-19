b0c9a0ba949ca57389765f7660d7a004f78686e5
Joao Moreira <joaomarcello.mm@gmail.com>
2024-02-19 12:26:41 -0300

    minor changes

--- main.lua
+++ main.lua
@@ -81,7 +81,7 @@ function love.load()
             self:set_shader(shader)
         end
     end)
-    return JM:load_initial_state("lib.gamestate.title", false)
+    return JM:load_initial_state("lib.gamestate.game", false)
     -- return JM:load_initial_state("jm-love2d-package.modules.editor.editor", false)
 end
 
