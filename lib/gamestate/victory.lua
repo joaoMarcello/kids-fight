1deedeb17b63497ec3da10e2345eb26784bec1d1
Joao Moreira <joaomarcello.mm@gmail.com>
2024-02-20 16:11:26 -0300

    add text info

--- lib/gamestate/victory.lua
+++ lib/gamestate/victory.lua
@@ -24,6 +24,8 @@ local State = JM.Scene:new {
     bound_bottom = 1366,
     cam_scale = 1,
 }
+
+State:set_color(JM_Utils:hex_to_rgba_float("2c2433"))
 --============================================================================
 ---@class GameState.Victory.Data
 local data = {}
@@ -35,7 +37,7 @@ function State:__get_data__()
 end
 
 local function load()
-
+    JM:get_font("pix8")
 end
 
 local function finish()
@@ -43,7 +45,11 @@ local function finish()
 end
 
 local function init(args)
+    local game = require "lib.gamestate.game"
+    local _data_ = game:__get_data__()
 
+    data.total_time = _data_.time_game
+    data.time_gamestate = 0.0
 end
 
 local function textinput(t)
@@ -51,9 +57,29 @@ local function textinput(t)
 end
 
 local function keypressed(key)
+    if State.transition or data.time_gamestate < 1 then return end
+
     if key == 'o' then
         State.camera:toggle_grid()
         State.camera:toggle_world_bounds()
+    end
+
+    local P1 = JM.ControllerManager.P1
+    local Button = P1.Button
+
+    if P1:pressed(Button.start, key) then
+        if not State.transition then
+            return State:add_transition("fade", "out", { post_delay = 0.2 }, nil,
+                ---@param State JM.Scene
+                function(State)
+                    return State:change_gamestate(
+                        require "lib.gamestate.title",
+                        {
+                            transition = "fade",
+                        }
+                    )
+                end)
+        end
     end
 end
 
@@ -97,11 +123,16 @@ local function resize(w, h)
 end
 
 local function update(dt)
-
+    data.time_gamestate = data.time_gamestate + dt
 end
 
 local function draw(cam)
-
+    local font = JM:get_font("pix8")
+    local Utils = JM_Utils
+    font:push()
+    font:set_color(Utils:get_rgba(Utils:hex_to_rgba_float("e5f285")))
+    font:printf(string.format("Your time was\n%.1f seconds", data.total_time), 0, 16 * 6, SCREEN_WIDTH, "center")
+    font:pop()
 end
 
 --============================================================================
