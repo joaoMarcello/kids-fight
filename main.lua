9496c93940177542eb13895e4671d0d70932cbf0
Joao Moreira <joaomarcello.mm@gmail.com>
2024-02-27 20:01:35 -0300

    dont allow toggle fullscreen if played on WEB

--- main.lua
+++ main.lua
@@ -121,6 +121,10 @@ function love.keypressed(key, scancode, isrepeat)
         return love.graphics.captureScreenshot("img_" .. os.time() .. ".png")
     end
 
+    if _G.WEB and scancode == 'f11' then
+        return
+    end
+
     if not _G.WEB and scancode == 'f10' then
         SAVE_DATA.skip_crt = not SAVE_DATA.skip_crt
         local scene = JM.SceneManager.scene
