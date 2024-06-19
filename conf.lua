5ae091b790ebd9fcbd1bcf5a481259d4c92cdb5e
Joao Moreira <joaomarcello.mm@gmail.com>
2024-06-19 17:36:27 -0300

    documentation and parry

--- conf.lua
+++ conf.lua
@@ -22,8 +22,8 @@ function love.conf(t)
     -- t.window.width = 975  --320 * 2.5 -- The window width (number)
     -- t.window.height = 450 -- The window height (number)
 
-    t.window.width = 320 * 2.5  -- The window width (number)
-    t.window.height = 180 * 2.5 -- The window height (number)
+    t.window.width = 320 * 3  -- The window width (number)
+    t.window.height = 180 * 3 -- The window height (number)
 
     -- t.window.width = 800
     -- t.window.height = 600
